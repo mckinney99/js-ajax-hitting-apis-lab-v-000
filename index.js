e32de666e3772a735e625e51d364c4108a7ecd02
Eric McKinney <mckinney99@gmail.com>
2018-03-07 20:07:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-
-function showRepositories(event, data) {
-  var repos = JSON.parse(this.responseText)
-  console.log(repos)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
-  document.getElementById("repositories").innerHTML = repoList
-}
-
 function getRepositories() {
   const req = new XMLHttpRequest()
   req.addEventListener("load", showRepositories)
@@ -13,6 +5,12 @@ function getRepositories() {
   req.send()
 }
 
+function showRepositories(event, data) {
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
 
 function getCommits(el) {
   const name = el.dataset.repo
