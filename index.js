47cb828c81b88e6f0bce6d738bf11a864a188214
Eric McKinney <mckinney99@gmail.com>
2018-03-07 20:07:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-function getRepositories() {
-  const req = new XMLHttpRequest()
-  req.addEventListener("load", showRepositories)
-  req.open("GET", 'https://api.github.com/users/octocat/repos')
-  req.send()
-}
 
 function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
@@ -11,6 +5,14 @@ function showRepositories(event, data) {
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
+
+function getRepositories() {
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories)
+  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.send()
+}
+
 
 function getCommits(el) {
   const name = el.dataset.repo
